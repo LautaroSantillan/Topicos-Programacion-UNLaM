fb3f6102cb2f5ff4c64150f660c341132880d323
Lautaro Ezequiel Santillan <santillanlautaro03@gmail.com>
2024-03-29 22:43:50 -0300

    Add files via upload

---
+++ Clase01/Ejercicios/funciones.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+///TDA: Tipo de Dato Abstracto
+
+typedef struct{
+    int dia;
+    int mes;
+    int anio;
+}Fecha;
+
+/// Primitivas
+
+Fecha ingresarFecha();
+bool esFechaValida(const Fecha* f);
+void mostrarFecha(const Fecha* f);
+
+/// Funciones a realizar
+
+Fecha sumarDiasAFecha(const Fecha* f, int dias); /// Listo
+Fecha restarDiasAFecha(const Fecha* f, int dias); // A revisar
+int difEntreFechas(const Fecha* f1, const Fecha* f2);
+int diaDelAnio(const Fecha* f); /// Listo
+Fecha diaDelAnioAFecha(int diaAnio, int anio); /// Listo
+int diaDeLaSemana(const Fecha* f); // 1 - Lunes, 2 - Martes, ... --> Revisar
+void determinarDiaSemana (int dia); /// Listo
---
+++ Clase01/Ejercicios/main.c
@@ -0,0 +1,66 @@
+#include "funciones.h"
+
+int main()
+{
+    ///Sumar dias a la fecha
+
+    /*int dias;
+    Fecha fechaIng = ingresarFecha();
+    mostrarFecha(&fechaIng);
+    puts("\nIngresar la cantidad de dias a sumar: ");
+    scanf("%d",&dias);
+    Fecha nFecha = sumarDiasAFecha(&fechaIng, dias);
+    mostrarFecha(&nFecha);*/
+
+// ------------------------------------------------------------------------------------
+
+    ///Restas dias a la fecha
+
+    /*int dias;
+    Fecha fechaIng = ingresarFecha();
+    mostrarFecha(&fechaIng);
+    puts("\nIngresar la cantidad de dias a restar: ");
+    scanf("%d",&dias);
+    Fecha nFecha = restarDiasAFecha(&fechaIng, dias);
+    mostrarFecha(&nFecha);*/
+
+// ------------------------------------------------------------------------------------
+
+    ///Obtener dia del año (en cantidad)
+
+    /*Fecha fechaIng = ingresarFecha();
+    mostrarFecha(&fechaIng);
+    int diaAnio = diaDelAnio(&fechaIng);
+    printf("\nLa cantidad de dias del anio es: %d", diaAnio);*/
+
+
+// ------------------------------------------------------------------------------------
+
+    ///Obtener la Fecha dependiendo del anio y la cantidad de dias
+
+    /*int diaAnio, anio;
+    puts("Ingresar la cantidad de dias del anio: ");
+    scanf("%d",&diaAnio);
+    while(diaAnio < 0 && diaAnio > 366)
+    {
+        puts("\nCantidad de dias del anio incorrecto. Ingrese nuevamente");
+        puts("Ingresar la cantidad de dias del anio: ");
+        scanf("%d",&diaAnio);
+    }
+    puts("Ingresar el anio: ");
+    scanf("%d",&anio);
+    Fecha fechaAnio = diaDelAnioAFecha(diaAnio, anio);
+    mostrarFecha(&fechaAnio);*/
+
+// -----------------------------------------------------------------------------------
+
+    ///Obtener el dia de la semana
+
+    /*Fecha fechaIng = ingresarFecha();
+    mostrarFecha(&fechaIng);
+    puts("\n");
+    int diaSemana = diaDeLaSemana(&fechaIng);
+    determinarDiaSemana(diaSemana);*/
+
+    return 0;
+}
