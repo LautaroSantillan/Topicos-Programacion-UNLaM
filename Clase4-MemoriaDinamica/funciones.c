0f64a023a935968a5d532562d11b792cac6ec03f
Lautaro Ezequiel Santillan <santillanlautaro03@gmail.com>
2024-04-21 23:13:14 -0300

    Add files via upload

--- Clase4-MemoriaDinamica/funciones.c
+++ Clase4-MemoriaDinamica/funciones.c
@@ -41,8 +41,8 @@ int vectorOrdInsertar(MiVector* vec, int elem)
     int* ultElem = vec->vec + vec->ce - 1;
     while(posIns <= ultElem && elem > *posIns)
         posIns++;
-    if(posIns <= ultElem && elem == *posIns)
-        return DUPLICADO;
+//    if(posIns <= ultElem && elem == *posIns)
+//        return DUPLICADO;
     for(int* i = ultElem; i >= posIns; i--)
         *(i+1) = *i; /// vec[i+1] = vec[i]
     *posIns = elem;
@@ -67,6 +67,140 @@ int vectorOrdEliminarElem(MiVector* vec, int elem)
     {
         vec->cap /= 2;
         vec->vec = realloc(vec->vec, vec->cap*sizeof(int));
+    }
+
+    return TODO_OK;
+}
+
+int vectorInsertarPrin(MiVector* vec, int elem)
+{
+    /// VERIFICAR SI ESTA LLENO EL VECTOR (SI ES ASI, SE INCREMENTA SU MEMORIA)
+    if(vec->ce == vec->cap)
+    {
+        int nuevaCap = vec->cap * 1.5;
+        int* nuevoVec = realloc(vec->vec, nuevaCap*sizeof(int));
+
+        if(!nuevoVec)
+            return SIN_MEM;
+        vec->vec = nuevoVec;
+        vec->cap = nuevaCap;
+    }
+    int* posIns = vec->vec;
+    int* ultElem = vec->vec + vec->ce - 1;
+    if(elem == *posIns)
+        return DUPLICADO;
+    for(int* i = ultElem; i >= posIns; i--)
+        *(i+1) = *i;
+    *posIns = elem;
+    vec->ce++;
+
+    return TODO_OK;
+}
+
+int vectorInsertarFin(MiVector* vec, int elem)
+{
+    /// VERIFICAR SI ESTA LLENO EL VECTOR (SI ES ASI, SE INCREMENTA SU MEMORIA)
+    if(vec->ce == vec->cap)
+    {
+        int nuevaCap = vec->cap * 1.5;
+        int* nuevoVec = realloc(vec->vec, nuevaCap*sizeof(int));
+
+        if(!nuevoVec)
+            return SIN_MEM;
+        vec->vec = nuevoVec;
+        vec->cap = nuevaCap;
+    }
+    int* ultElem = vec->vec + vec->ce - 1;
+    int* posIns = ultElem + 1;
+    if(elem == *ultElem)
+        return DUPLICADO;
+    *posIns = elem;
+    vec->ce++;
+
+    return TODO_OK;
+}
+
+int vectorInsertarCualPos(MiVector* vec, int elem, int pos)
+{
+    /// VERIFICAR SI ESTA LLENO EL VECTOR (SI ES ASI, SE INCREMENTA SU MEMORIA)
+    if(vec->ce == vec->cap)
+    {
+        int nuevaCap = vec->cap * 1.5;
+        int* nuevoVec = realloc(vec->vec, nuevaCap*sizeof(int));
+
+        if(!nuevoVec)
+            return SIN_MEM;
+        vec->vec = nuevoVec;
+        vec->cap = nuevaCap;
+    }
+    int* posIns = vec->vec + pos;
+    int* ultElem = vec->vec + vec->ce - 1;
+    if(elem == *posIns)
+        return DUPLICADO;
+    for(int* i = ultElem; i >= posIns; i--)
+        *(i+1) = *i;
+    *posIns = elem;
+    vec->ce++;
+
+    return TODO_OK;
+}
+
+int vectorOrdBuscarElem(MiVector* vec, int elem)
+{
+    int* inicio = vec->vec;
+    int* fin = vec->vec + vec->ce - 1;
+    while(inicio<fin)
+    {
+        if(elem == *inicio)
+        {
+            puts("Se encontro el elemento");
+            return TODO_OK;
+        }
+        inicio++;
+    }
+    puts("No se encontro el elemento");
+    return ERROR;
+}
+
+int vectorEliminarPos(MiVector* vec, int pos)
+{
+    int* posElim = vec->vec + pos;
+    int* ultElem = vec->vec + vec->ce - 1;
+    for(int* i = posElim; i < ultElem; i++)
+        *i = *(i+1);
+    vec->ce--;
+
+    return TODO_OK;
+}
+
+int vectorEliminarPri(MiVector* vec)
+{
+    int* ultElem = vec->vec + vec->ce - 1;
+    for(int* i = vec->vec; i < ultElem; i++)
+        *i = *(i+1);
+    vec->ce--;
+
+    return TODO_OK;
+}
+
+int vectorEliminarUlt(MiVector* vec)
+{
+    vec->ce--;
+
+    return TODO_OK;
+}
+
+int vectorOrdEliminarDup(MiVector* vec, int elem)
+{
+    int* inicio = vec->vec;
+    int* fin = vec->vec + vec->ce - 1;
+    for(int* i = inicio; i < fin; i++)
+    {
+        if(*i == elem)
+        {
+            *i = *(i+1);
+            vec->ce--;
+        }
     }
 
     return TODO_OK;
--- Clase4-MemoriaDinamica/funciones.h
+++ Clase4-MemoriaDinamica/funciones.h
@@ -2,21 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define TAM 100
-#define TAM_2 200
 #define SIN_MEM 0
 #define TODO_OK 1
 #define DUPLICADO 2
 #define ERROR 3
 
-typedef struct
-{
+typedef struct{
     int* vec;
     int ce; // cant. de elementos cargados
     int cap; // cant. de elementos totales que se quiere disponer
-}
-MiVector;
-
+}MiVector;
 
 ///void* malloc(size_t cantBytes);
 ///void* callloc(void*, size_t);
@@ -25,18 +20,13 @@ MiVector;
 bool vectorCrear(MiVector* vec);
 void mostrar(MiVector *vec);
 int vectorOrdInsertar(MiVector* vec, int);
+int vectorOrdBuscarElem(MiVector* vec, int);
 int vectorOrdEliminarElem(MiVector* vec, int);
-//buscar con aritmetica de punteros
-/*int vectorInsertarPrin(Vector* vec, int);
-int vectorInsertarFin(Vector* vec, int);
-int vectorInsertarCualPos(Vector* vec, int, int);
-int vectorEliminarPos(Vector* vec, int);
-int vectorEliminarPri(Vector* vec);
-int vectorEliminarUlt(Vector* vec);
-int vectorEliminarValor(Vector* vec, int);
-int vectorBuscarElemDes(Vector* vec, int elem);
-/// ORDENADO
-int vectorInsertarOrd(Vector* vec, int);
-int vectorBuscarElemOrd(Vector* vec, int elem);
-int vectorEliminarValorOrd(Vector* vec, int elem);
-int vectorEliminarDupOrd(Vector* vec, int elem);*/
+int vectorInsertarPrin(MiVector* vec, int);
+int vectorInsertarFin(MiVector* vec, int);
+int vectorInsertarCualPos(MiVector* vec, int, int);
+int vectorEliminarPos(MiVector* vec, int);
+int vectorEliminarPri(MiVector* vec);
+int vectorEliminarUlt(MiVector* vec);
+int vectorOrdEliminarDup(MiVector* vec, int);
+///int vectorBuscarElemDes(MiVector* vec, int elem);
